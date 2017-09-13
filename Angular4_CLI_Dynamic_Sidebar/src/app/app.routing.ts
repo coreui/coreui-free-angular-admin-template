9cac486d8c3a19d043af8adc4d83b80ef798e928
Wangkanai <sarin@wangkanai.com>
2017-09-13 14:23:01 +0700

    first commit

---
+++ Angular4_CLI_Dynamic_Sidebar/src/app/app.routing.ts
@@ -0,0 +1,64 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Import Containers
+import {
+  FullLayout,
+  SimpleLayout
+} from './containers';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: FullLayout,
+    data: {
+      title: 'Home'
+    },
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: './views/dashboard/dashboard.module#DashboardModule'
+      },
+      {
+        path: 'components',
+        loadChildren: './views/components/components.module#ComponentsModule'
+      },
+      {
+        path: 'icons',
+        loadChildren: './views/icons/icons.module#IconsModule'
+      },
+      {
+        path: 'widgets',
+        loadChildren: './views/widgets/widgets.module#WidgetsModule'
+      },
+      {
+        path: 'charts',
+        loadChildren: './views/chartjs/chartjs.module#ChartJSModule'
+      }
+    ]
+  },
+  {
+    path: 'pages',
+    component: SimpleLayout,
+    data: {
+      title: 'Pages'
+    },
+    children: [
+      {
+        path: '',
+        loadChildren: './views/pages/pages.module#PagesModule',
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
---
+++ Angular4_CLI_Dynamic_Sidebar/src/app/components/index.ts
@@ -0,0 +1,5 @@
+export * from './app-aside';
+export * from './app-breadcrumbs';
+export * from './app-footer';
+export * from './app-header';
+export * from './app-sidebar';
