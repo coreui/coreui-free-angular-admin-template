d10bd345e5defcf138afc4bc0fd35107e0cffc92
xidedix <xidedix@users.noreply.github.com>
2018-02-02 19:00:50 +0100

    v1.0.10

--- Angular4_CLI_Starter/src/app/components/app-sidebar-footer/app-sidebar-footer.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-sidebar-footer',
-  templateUrl: './app-sidebar-footer.component.html'
-})
-export class AppSidebarFooterComponent { }
--- Angular4_CLI_Starter/src/app/components/app-sidebar/app-sidebar.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-sidebar',
-  templateUrl: './app-sidebar.component.html'
-})
-export class AppSidebarComponent { }
