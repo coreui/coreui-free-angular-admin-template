28d31945eef8d79bf9c316c7ca15185b99aee2e2
xidedix <xidedix@users.noreply.github.com>
2018-04-30 16:28:06 +0200

    refactor: ie10 MutationObserver polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -45,6 +45,8 @@ import 'core-js/es6/reflect';
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 import 'core-js/es7/reflect';
 
+// for IE10
+import 'mutationobserver-shim'
 
 /**
  * Required to support Web Animations `@angular/platform-browser/animations`.
