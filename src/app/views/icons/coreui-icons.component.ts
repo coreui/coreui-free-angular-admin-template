482f6590f256a76a4e0b5cbd8a17a83f330f45d9
Łukasz Holeczek <lukasz@holeczek.pl>
2018-05-09 17:43:13 +0200

    feat: coreui icons

---
+++ src/app/views/icons/coreui-icons.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'coreui-icons.component.html'
+})
+export class CoreUIIconsComponent {
+
+  constructor() { }
+
+}
--- src/app/views/icons/icons.module.ts
+++ src/app/views/icons/icons.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 
+import { CoreUIIconsComponent } from './coreui-icons.component';
 import { FlagsComponent } from './flags.component';
 import { FontAwesomeComponent } from './font-awesome.component';
 import { SimpleLineIconsComponent } from './simple-line-icons.component';
@@ -9,6 +10,7 @@ import { IconsRoutingModule } from './icons-routing.module';
 @NgModule({
   imports: [ IconsRoutingModule ],
   declarations: [
+    CoreUIIconsComponent,
     FlagsComponent,
     FontAwesomeComponent,
     SimpleLineIconsComponent
