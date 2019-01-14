f956944ec66c25ebc894498684d6ae96526765ef
xidedix <xidedix@users.noreply.github.com>
2019-01-14 17:45:17 +0100

    refactor(_nav.ts): add NavData interface

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -1,4 +1,17 @@
-export const navItems = [
+export interface NavData {
+  name?: string,
+  url?: string,
+  icon?: string,
+  badge?: any,
+  title?: boolean,
+  children?: any,
+  variant?: string,
+  attributes?: object,
+  divider?: boolean,
+  class?: string,
+}
+
+export const navItems: NavData[] = [
   {
     name: 'Dashboard',
     url: '/dashboard',
