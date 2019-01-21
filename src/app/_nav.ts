d6d46f5b412c24efad5302168406cd732101ca05
xidedix <xidedix@users.noreply.github.com>
2019-01-21 15:45:43 +0100

    refactor(_nav.ts): NavData interface semicolons

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -1,14 +1,14 @@
 export interface NavData {
-  name?: string,
-  url?: string,
-  icon?: string,
-  badge?: any,
-  title?: boolean,
-  children?: any,
-  variant?: string,
-  attributes?: object,
-  divider?: boolean,
-  class?: string,
+  name?: string;
+  url?: string;
+  icon?: string;
+  badge?: any;
+  title?: boolean;
+  children?: any;
+  variant?: string;
+  attributes?: object;
+  divider?: boolean;
+  class?: string;
 }
 
 export const navItems: NavData[] = [
