aa29476b5ba24479c7570b0e4abcdc9bcb8fa453
xidedix <xidedix@users.noreply.github.com>
2019-03-20 16:20:06 +0100

    refactor(modals): add @ViewChild

--- src/app/views/notifications/modals.component.ts
+++ src/app/views/notifications/modals.component.ts
@@ -1,16 +1,16 @@
-import { Component, ViewChild } from '@angular/core';
-import { ModalDirective } from 'ngx-bootstrap/modal';
+import {Component, ViewChild} from '@angular/core';
+import {ModalDirective} from 'ngx-bootstrap/modal';
 
 @Component({
   templateUrl: 'modals.component.html'
 })
 export class ModalsComponent {
-    public myModal;
-    public largeModal;
-    public smallModal;
-    public primaryModal;
-    public successModal;
-    public warningModal;
-    public dangerModal;
-    public infoModal;
+  @ViewChild('myModal') public myModal: ModalDirective;
+  @ViewChild('largeModal') public largeModal: ModalDirective;
+  @ViewChild('smallModal') public smallModal: ModalDirective;
+  @ViewChild('primaryModal') public primaryModal: ModalDirective;
+  @ViewChild('successModal') public successModal: ModalDirective;
+  @ViewChild('warningModal') public warningModal: ModalDirective;
+  @ViewChild('dangerModal') public dangerModal: ModalDirective;
+  @ViewChild('infoModal') public infoModal: ModalDirective;
 }
