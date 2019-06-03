60ce7b2a094187305545debd00561029d45be942
xidedix <xidedix@users.noreply.github.com>
2019-06-03 17:38:38 +0200

    fix(polyfill): import `web-animations-js`

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -49,7 +49,7 @@ import 'mutationobserver-shim';
  * Required to support Web Animations `@angular/platform-browser/animations`.
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
  **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
@@ -59,7 +59,6 @@ import 'mutationobserver-shim';
  (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
  (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
  (window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
- (window as any).__Zone_enable_cross_context_check = true;
 
  /*
  * in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
