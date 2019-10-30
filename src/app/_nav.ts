d77180f08fe41bc020cdaaaae5f532eaeb0fdc0d
xidedix <xidedix@users.noreply.github.com>
2019-10-30 13:39:58 +0100

    refactor(_nav): INavData moved from _nav.ts to import from @coreui/angular

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -1,35 +1,6 @@
-interface NavAttributes {
-  [propName: string]: any;
-}
-interface NavWrapper {
-  attributes: NavAttributes;
-  element: string;
-}
-interface NavBadge {
-  text: string;
-  variant: string;
-}
-interface NavLabel {
-  class?: string;
-  variant: string;
-}
+import { INavData } from '@coreui/angular';
 
-export interface NavData {
-  name?: string;
-  url?: string;
-  icon?: string;
-  badge?: NavBadge;
-  title?: boolean;
-  children?: NavData[];
-  variant?: string;
-  attributes?: NavAttributes;
-  divider?: boolean;
-  class?: string;
-  label?: NavLabel;
-  wrapper?: NavWrapper;
-}
-
-export const navItems: NavData[] = [
+export const navItems: INavData[] = [
   {
     name: 'Dashboard',
     url: '/dashboard',
