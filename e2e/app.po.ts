ac237c4d9db88d212b643ba359d02a0dc30c9be5
xidedix <xidedix@users.noreply.github.com>
2019-10-30 13:41:03 +0100

    test: add e2e test bed with some tests

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,25 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class CoreUIPage {
+export class AppDashboard {
+
+  getBrowser() {
+    return browser;
+  }
+
   navigateTo() {
     return browser.get('/');
   }
 
   getParagraphText() {
+    return element(by.xpath('/html/body/app-dashboard/div/main/div/ng-component/div/div[2]/div[1]/div[1]/div[1]/h4')).getText();
+  }
+  getBody() {
+    return element(by.xpath('/html/body'));
+  }
+  getByCss(selector) {
+    return element.all(by.css(selector));
+  }
+  getFooterText() {
     return element(by.className('app-footer')).getText();
   }
 }
