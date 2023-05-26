dd1291faf353f355da265ee37c44d584221f7a97
xidedix <xidedix@users.noreply.github.com>
2023-05-26 21:30:26 +0200

    refactor: minor fixes

--- src/app/views/buttons/dropdowns/dropdowns.component.ts
+++ src/app/views/buttons/dropdowns/dropdowns.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dropdowns',
   templateUrl: './dropdowns.component.html',
-  styleUrls: ['./dropdowns.component.scss']
 })
 export class DropdownsComponent {
 
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,5 +1,3 @@
-declare module '@coreui/chartjs/dist/js/coreui-chartjs.js';
-
 declare module '*.json' {
   const value: any;
   export default value;
