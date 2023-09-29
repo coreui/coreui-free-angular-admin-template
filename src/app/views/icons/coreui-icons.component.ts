dd7cef79b8a6fc32c846f280460ebc167e9bf1c9
TramAnhDinh <dinhnguyentramanh123@gmail.com>
2023-09-29 16:39:51 +0700

    change

--- src/app/views/icons/coreui-icons.component.ts
+++ src/app/views/icons/coreui-icons.component.ts
@@ -21,17 +21,17 @@ export class CoreUIIconsComponent implements OnInit {
   ngOnInit() {
     const path = this.route?.routeConfig?.path;
     let prefix = 'cil';
-    if (path === 'coreui-icons') {
-      this.title = `${this.title} - Free`;
-      prefix = 'cil';
-    } else if (path === 'brands') {
-      this.title = `${this.title} - Brands`;
-      prefix = 'cib';
-    } else if (path === 'flags') {
-      this.title = `${this.title} - Flags`;
-      prefix = 'cif';
-    }
-    this.icons = this.getIconsView(prefix);
+    // if (path === 'coreui-icons') {
+    //   this.title = `${this.title} - Free`;
+    //   prefix = 'cil';
+    // } else if (path === 'brands') {
+    //   this.title = `${this.title} - Brands`;
+    //   prefix = 'cib';
+    // } else if (path === 'flags') {
+    //   this.title = `${this.title} - Flags`;
+    //   prefix = 'cif';
+    // }
+    // this.icons = this.getIconsView(prefix);
   }
 
   toKebabCase(str: string) {
