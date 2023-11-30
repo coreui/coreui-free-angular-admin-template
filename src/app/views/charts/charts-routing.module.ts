386500d80e4510e6b4f5112045aacc168282a60f
Macombija <miyomarenhard@gmail.com>
2023-11-30 08:47:33 +0300

    update sidebar

--- src/app/views/charts/charts-routing.module.ts
+++ src/app/views/charts/charts-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { ChartsComponent } from './charts.component';
 
 const routes: Routes = [
