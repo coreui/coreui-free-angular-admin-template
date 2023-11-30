40909a98fa6157ab716128f6f9945575f17df0d4
Renhard Ombija Miyoma <miyomarenhard@gmail.com>
2023-11-30 15:20:55 +0300

    ADD

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -5,6 +5,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['dashboard.component.scss']
 })
 export class DashboardComponent {
+  // dashboard items
+  dashboard1 = "Monthly Scorecard"
+  dashboard2 = "Initiative Performance"
+  dashboard3 = "Total Initiatives"
+  dashboard4 = "Budget Utilization"
+  // cards names
+  card1 = "PROFILES"
+  card2 = "INVESTIGATION"
+  card3 = "PROSECUTION"
+  card4 = "OBJECTION REVIEW"
+  card5 = "ADR"
+  card = " DEBT"
+
   constructor() {
   }
   
