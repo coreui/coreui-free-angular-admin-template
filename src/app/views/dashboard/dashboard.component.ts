b5912934d47e2f8fd57d04711e2f5b62f67059e1
Renhard Ombija Miyoma <miyomarenhard@gmail.com>
2023-12-01 09:52:46 +0300

    updated cards

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { cilArrowTop, cilOptions } from '@coreui/icons';
-import { getStyle } from '@coreui/utils';
 
 @Component({
   templateUrl: 'dashboard.component.html',
@@ -18,86 +17,28 @@ export class DashboardComponent implements OnInit {
   card3 = "PROSECUTION"
   card4 = "OBJECTION REVIEW"
   card5 = "ADR"
-  card = " DEBT"
+  card6 = " DEBT"
 
   icons = { cilOptions, cilArrowTop };
 
-  data: any = {};
-  options: any = {};
-
-  labels = [
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-  ];
-
-  datasets = [
-    {
-      label: 'My First dataset',
-      backgroundColor: 'transparent',
-      borderColor: 'rgba(255,255,255,.55)',
-      pointBackgroundColor: getStyle('--cui-primary'),
-      pointHoverBorderColor: getStyle('--cui-primary'),
-      data: [65, 59, 84, 84, 51, 55, 40, 59, 84, 84, 51, 55]
+  getProgressBarColor(percentage: number): string {
+    if (percentage > 90) {
+      return 'green';
+    } else if (percentage > 80) {
+      return 'orange';
+    } else {
+      return 'red';
     }
-  ];
-
-  optionsDefault = {
-    plugins: {
-      legend: {
-        display: false
-      }
-    },
-    maintainAspectRatio: true,
-    scales: {
-      x: {
-        grid: {
-          display: false,
-          drawBorder: false
-        },
-        ticks: {
-          display: false
-        }
-      },
-      y: {
-        min: 30,
-        max: 89,
-        display: false,
-        grid: {
-          display: false
-        },
-        ticks: {
-          display: false
-        }
-      }
-    },
-    elements: {
-      line: {
-        borderWidth: 1,
-        tension: 0.4
-      },
-      point: {
-        radius: 4,
-        hitRadius: 10,
-        hoverRadius: 4
-      }
-    }
-  };
-
-  ngOnInit(): void {
-    this.data = {
-      labels: this.labels.slice(0, 7),
-      datasets: this.datasets
-    };
-    this.options = this.optionsDefault;
   }
+
+  progress: number = 0;
+
+  updateProgress() {
+    this.progress += 10;
+    if (this.progress > 100) {
+      this.progress = 0;
+    }
+  }
+
+  ngOnInit() {}
 }
