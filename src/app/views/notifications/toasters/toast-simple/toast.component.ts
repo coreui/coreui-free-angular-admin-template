7671b15e970bc41398fe1ff19c08b59e7c0dbb95
xidedix <xidedix@users.noreply.github.com>
2024-12-06 23:47:00 +0100

    fix(toast.component): remove constructor-based dependency injection

--- src/app/views/notifications/toasters/toast-simple/toast.component.ts
+++ src/app/views/notifications/toasters/toast-simple/toast.component.ts
@@ -1,25 +1,20 @@
-import { ChangeDetectorRef, Component, ElementRef, forwardRef, Input, Renderer2 } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
 
-import { ToastComponent, ToasterService, ToastHeaderComponent, ToastBodyComponent, ToastCloseDirective, ProgressBarDirective, ProgressComponent } from '@coreui/angular';
+import { ProgressComponent, ToastBodyComponent, ToastCloseDirective, ToastComponent, ToastHeaderComponent } from '@coreui/angular';
 
 @Component({
-    selector: 'app-toast-simple',
-    templateUrl: './toast.component.html',
-    styleUrls: ['./toast.component.scss'],
-    providers: [{ provide: ToastComponent, useExisting: forwardRef(() => AppToastComponent) }],
-    imports: [ToastHeaderComponent, ToastBodyComponent, ToastCloseDirective, ProgressBarDirective, ProgressComponent]
+  selector: 'app-toast-simple',
+  templateUrl: './toast.component.html',
+  styleUrls: ['./toast.component.scss'],
+  providers: [{ provide: ToastComponent, useExisting: forwardRef(() => AppToastComponent) }],
+  imports: [ToastHeaderComponent, ToastBodyComponent, ToastCloseDirective, ProgressComponent]
 })
 export class AppToastComponent extends ToastComponent {
 
+  constructor() {
+    super();
+  }
+
   @Input() closeButton = true;
   @Input() title = '';
-
-  constructor(
-    public override hostElement: ElementRef,
-    public override renderer: Renderer2,
-    public override toasterService: ToasterService,
-    public override changeDetectorRef: ChangeDetectorRef
-  ) {
-    super(hostElement, renderer, toasterService, changeDetectorRef);
-  }
 }
