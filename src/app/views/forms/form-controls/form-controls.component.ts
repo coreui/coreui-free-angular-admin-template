c9257d8fdc7f5fe192d1f0062fd84f901ad6a501
xidedix <xidedix@users.noreply.github.com>
2025-02-07 21:31:30 +0100

    refactor(form-control): cleanup readonly / plaintext example

--- src/app/views/forms/form-controls/form-controls.component.ts
+++ src/app/views/forms/form-controls/form-controls.component.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
 import { NgStyle } from '@angular/common';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DocsExampleComponent } from '@docs-components/public-api';
-import { RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, FormDirective, FormLabelDirective, FormControlDirective, ButtonDirective } from '@coreui/angular';
+import {
+  ButtonDirective,
+  CardBodyComponent,
+  CardComponent,
+  CardHeaderComponent,
+  ColComponent,
+  ColDirective,
+  FormControlDirective,
+  FormDirective,
+  FormLabelDirective,
+  GutterDirective,
+  RowComponent,
+  RowDirective,
+  TextColorDirective
+} from '@coreui/angular';
 
 @Component({
     selector: 'app-form-controls',
     templateUrl: './form-controls.component.html',
     styleUrls: ['./form-controls.component.scss'],
-    imports: [RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, DocsExampleComponent, ReactiveFormsModule, FormsModule, FormDirective, FormLabelDirective, FormControlDirective, ButtonDirective, NgStyle]
+  imports: [RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, DocsExampleComponent, ReactiveFormsModule, FormsModule, FormDirective, FormLabelDirective, FormControlDirective, ButtonDirective, NgStyle, RowDirective, GutterDirective, ColDirective]
 })
 export class FormControlsComponent {
 
   public favoriteColor = '#26ab3c';
-
-  constructor() { }
-
+  
 }
