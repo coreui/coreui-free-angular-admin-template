1886a3912d6078584b3e7750d3d5e0e356b5d0ce
xidedix <xidedix@users.noreply.github.com>
2025-08-18 10:57:20 +0200

    test: drop deprecated BrowserDynamicTestingModule, use BrowserTestingModule and platformBrowserTesting()

--- src/test.ts
+++ src/test.ts
@@ -2,14 +2,11 @@
 
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(),
-  { teardown: { destroyAfterEach: true }},
+  BrowserTestingModule,
+  platformBrowserTesting(),
+  { teardown: { destroyAfterEach: true } }
 );
